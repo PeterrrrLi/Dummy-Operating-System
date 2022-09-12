a2b43db855328f5119f3b1ed930c69e971894a2d
j2457li <j2457li@uwaterloo.ca>
2022-09-12 12:02:11 -0400

    [0912] Problem 1 of A0

--- kern/startup/main.c
+++ kern/startup/main.c
@@ -101,7 +101,7 @@ boot(void)
 	kprintf("%s", harvard_copyright);
 	kprintf("\n");
 
-	kprintf("Put-your-group-name-here's system version %s (%s #%d)\n", 
+	kprintf("Peter Li's system version %s (%s #%d)\n", 
 		GROUP_VERSION, buildconfig, buildversion);
 	kprintf("\n");
 
