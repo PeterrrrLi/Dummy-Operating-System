b81980287b90908659abc52176f2806136304859
Peter Li <j2457li@uwaterloo.ca>
2022-10-05 14:48:22 -0400

    [1005] 119 score solution to traffic synch problem

--- kern/synchprobs/traffic_synch.c
+++ kern/synchprobs/traffic_synch.c
@@ -21,8 +21,15 @@
 /*
  * replace this with declarations of any synchronization and other variables you need here
  */
-static struct semaphore *intersectionSem;
-
+static struct lock *intersection_lock;
+static struct cv *intersection_cv;
+static int num_in_intersection;
+struct intersection_car {
+    Direction origin;
+    Direction destination;
+    struct intersection_car *next;
+};
+static struct intersection_car *intersection_list;
 
 /* 
  * The simulation driver will call this function once before starting
@@ -34,12 +41,19 @@ static struct semaphore *intersectionSem;
 void
 intersection_sync_init(void)
 {
-  /* replace this default implementation with your own implementation */
-
-  intersectionSem = sem_create("intersectionSem",1);
-  if (intersectionSem == NULL) {
-    panic("could not create intersection semaphore");
+  intersection_lock = lock_create("Lock for Intersection");
+  if (intersection_lock == NULL) {
+      kprintf("Error: Lock for intersection was not initialized");
+      return;
   }
+
+  intersection_cv = cv_create("Conditional variable for intersection");
+  if (intersection_cv == NULL) {
+      kprintf("Error: CV for intersection was not initialized");
+      return;
+  }
+  intersection_list = NULL;
+  num_in_intersection = 0;
   return;
 }
 
@@ -54,9 +68,32 @@ void
 intersection_sync_cleanup(void)
 {
   /* replace this default implementation with your own implementation */
-  KASSERT(intersectionSem != NULL);
-  sem_destroy(intersectionSem);
+  KASSERT(intersection_lock != NULL);
+  KASSERT(intersection_cv != NULL);
+  KASSERT(intersection_list == NULL);
+  lock_destroy(intersection_lock);
+  cv_destroy(intersection_cv);
 }
+
+bool if_a_car_is_making_a_right_turn(struct intersection_car *car_1);
+
+bool if_a_car_is_making_a_right_turn(struct intersection_car *car_1) {
+  if (((car_1->origin == west) && (car_1->destination == south)) ||
+      ((car_1->origin == north) && (car_1->destination == west)) ||
+      ((car_1->origin == east) && (car_1->destination == north)) ||
+      ((car_1->origin == south) && (car_1->destination == east))) {return true;}
+      else {return false;}
+}
+
+bool if_two_cars_can_be_in_intersection_together(struct intersection_car *car_1, struct intersection_car *car_2);
+
+bool if_two_cars_can_be_in_intersection_together(struct intersection_car *car_1, struct intersection_car *car_2) {
+  if (car_1->origin == car_2->origin) return true;
+  else if ((car_1->origin == car_2->destination) && (car_1->destination == car_2->origin)) return true;
+  else if ((car_1->destination != car_2->destination) && (if_a_car_is_making_a_right_turn(car_1) || if_a_car_is_making_a_right_turn(car_2))) return true;
+  else return false;
+}
+
 
 
 /*
@@ -75,11 +112,43 @@ intersection_sync_cleanup(void)
 void
 intersection_before_entry(Direction origin, Direction destination) 
 {
-  /* replace this default implementation with your own implementation */
-  (void)origin;  /* avoid compiler complaint about unused parameter */
-  (void)destination; /* avoid compiler complaint about unused parameter */
-  KASSERT(intersectionSem != NULL);
-  P(intersectionSem);
+  // /* replace this default implementation with your own implementation */
+  // (void)origin;  /* avoid compiler complaint about unused parameter */
+  // (void)destination; /* avoid compiler complaint about unused parameter */
+  // KASSERT(intersectionSem != NULL);
+  // P(intersectionSem);
+  KASSERT(intersection_lock != NULL);
+  KASSERT(intersection_cv != NULL);
+
+  struct intersection_car *new_car = kmalloc(sizeof(struct intersection_car));
+  if (new_car == NULL) {
+      panic("Error: New car was not initialized");
+  }
+  new_car->origin = origin;
+  new_car->destination = destination;
+  new_car->next = NULL;
+
+  lock_acquire(intersection_lock);
+
+  if (intersection_list == NULL) {
+    intersection_list = new_car;
+    KASSERT(num_in_intersection == 0);
+    num_in_intersection = 1;
+  } else {
+    struct intersection_car *loop_ptr = intersection_list;
+    while (loop_ptr != NULL) {
+      if (if_two_cars_can_be_in_intersection_together(loop_ptr, new_car)) {
+        loop_ptr = loop_ptr->next;
+      } else {
+        cv_wait(intersection_cv, intersection_lock);
+        loop_ptr = intersection_list;
+      }
+    }
+    new_car->next = intersection_list;
+    intersection_list = new_car;
+    num_in_intersection = num_in_intersection + 1;
+  }
+  lock_release(intersection_lock);
 }
 
 
@@ -97,9 +166,36 @@ intersection_before_entry(Direction origin, Direction destination)
 void
 intersection_after_exit(Direction origin, Direction destination) 
 {
-  /* replace this default implementation with your own implementation */
-  (void)origin;  /* avoid compiler complaint about unused parameter */
-  (void)destination; /* avoid compiler complaint about unused parameter */
-  KASSERT(intersectionSem != NULL);
-  V(intersectionSem);
+  // /* replace this default implementation with your own implementation */
+  // (void)origin;  /* avoid compiler complaint about unused parameter */
+  // (void)destination; /* avoid compiler complaint about unused parameter */
+  // KASSERT(intersectionSem != NULL);
+  // V(intersectionSem);
+  KASSERT(intersection_lock != NULL);
+  KASSERT(intersection_cv != NULL);
+
+  // acquire lock to modify intersection
+  lock_acquire(intersection_lock);
+  // first, find this car
+  struct intersection_car *loop_ptr_1 = intersection_list;
+  struct intersection_car *loop_ptr_2 = NULL;
+  while (loop_ptr_1 != NULL) {
+    // if this is our car
+    if ((loop_ptr_1->origin == origin) && (loop_ptr_1->destination == destination)) {
+      if (loop_ptr_2 != NULL) {
+        loop_ptr_2->next = loop_ptr_1->next;
+      } else { // if our car is the first car in our intersection car list
+        intersection_list = loop_ptr_1->next;
+      }
+      KASSERT(loop_ptr_1 != NULL);
+      kfree(loop_ptr_1); // free this car
+      num_in_intersection = num_in_intersection - 1;
+      cv_broadcast(intersection_cv, intersection_lock); // wake up all waiting cars
+      lock_release(intersection_lock); // give up the lock for the intersection
+      return;
+    } else {
+      loop_ptr_2 = loop_ptr_1;
+      loop_ptr_1 = loop_ptr_1->next;
+    }
+  }
 }
